48b812126bec27e855bbfb61e27e632071c6ba6f
john <tripham178@gmail.com>
2019-06-11 15:19:25 +0700

    search contacts and history

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -11,11 +11,23 @@ class ContactsPage extends StatefulWidget {
 
 class _ContactsPageState extends State<ContactsPage> {
   FirebaseUser currentUser;
-  final searchController = TextEditingController();
+  TextEditingController searchController = TextEditingController();
+  String txtSearchContact = "";
   @override
   void initState() {
+    searchController.addListener(() {
+      setState(() {
+        txtSearchContact = searchController.text;
+      });
+    });
     super.initState();
     _getCurrentUser();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -28,13 +40,24 @@ class _ContactsPageState extends State<ContactsPage> {
           title: Container(
             margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
             child: TextField(
+              enableInteractiveSelection: false,
+              controller: searchController,
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Color.fromRGBO(255, 255, 255, .45),
                 hintText: "Type a text",
                 contentPadding: EdgeInsets.fromLTRB(15.0, 0, 0, 0),
                 hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                suffixIcon: Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon:
+                      Icon(txtSearchContact == "" ? Icons.search : Icons.close),
+                  onPressed: () {
+                    setState(() {
+                      txtSearchContact = "";
+                    });
+                    searchController.clear();
+                  },
+                ),
                 border: new OutlineInputBorder(
                   borderRadius: BorderRadius.all(
                     Radius.circular(24.0),
@@ -51,10 +74,21 @@ class _ContactsPageState extends State<ContactsPage> {
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
-                itemCount: snapshot.data.documents.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    _buildListItem(context, snapshot.data.documents[index]),
-              );
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (txtSearchContact == "") {
+                      return _buildListItem(
+                          context, snapshot.data.documents[index]);
+                    } else {
+                      if (snapshot.data.documents[index].data["userName"]
+                          .contains(txtSearchContact)) {
+                        return _buildListItem(
+                            context, snapshot.data.documents[index]);
+                      } else {
+                        return Container();
+                      }
+                    }
+                  });
             } else {
               return CircularProgressIndicator();
             }
