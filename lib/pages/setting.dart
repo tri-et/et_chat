1df820f315b6830dad8316a4bbdf50e88b25842f
john <tripham178@gmail.com>
2019-06-11 15:57:29 +0700

    change ui setting page

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -5,6 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../theme.dart';
+
 class SettingPage extends StatefulWidget {
   @override
   _SettingPageState createState() => _SettingPageState();
@@ -33,7 +35,6 @@ class _SettingPageState extends State<SettingPage> {
         Container(
           margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
           child: RaisedButton(
-            child: Text("Sign Out"),
             onPressed: () {
               Map<String, String> userInfo = new HashMap();
               userInfo["status"] = "offline";
@@ -44,6 +45,28 @@ class _SettingPageState extends State<SettingPage> {
               FirebaseAuth.instance.signOut();
               Navigator.pushReplacementNamed(context, "/");
             },
+            padding: EdgeInsets.fromLTRB(25.0, 11.0, 25.0, 11.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(
+                  Icons.power_settings_new,
+                  color: Color.fromRGBO(28, 28, 28, .46),
+                  size: 24.0,
+                ),
+                Text(
+                  "SIGN OUT",
+                  style: TextStyle(
+                      color: Color.fromRGBO(245, 238, 238, 1),
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            color: secondary,
           ),
         ),
       ],
