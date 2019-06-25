ec25adcfc187c4f867d36131937387bfeda32c9a
John <tripham178@gmail.com>
2019-06-25 13:22:52 +0700

    modify modal avatar

--- lib/widgets/avatarModal.dart
+++ lib/widgets/avatarModal.dart
@@ -8,31 +8,22 @@ class AvatarModal {
           return Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              _createTile(context, 'View Profile Picture', Icons.account_circle,
-                  _action1),
-              _createTile(context, 'Select Profile Picture', Icons.camera_alt,
-                  _action2),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('View Profile Picture'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.camera_alt),
+                title: Text('Select Profile Picture'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
             ],
           );
         });
-  }
-
-  ListTile _createTile(
-      BuildContext context, String name, IconData icon, Function action) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(name),
-      onTap: () {
-        Navigator.pop(context);
-      },
-    );
-  }
-
-  _action1() {
-    print('action 1');
-  }
-
-  _action2() {
-    print('action 2');
   }
 }
