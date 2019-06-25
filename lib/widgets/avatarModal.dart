ae4a0ee0e559ca8fd2d5305bc884839a6fb2975f
John <tripham178@gmail.com>
2019-06-25 10:08:21 +0700

    show avatar

--- lib/widgets/avatarModal.dart
+++ lib/widgets/avatarModal.dart
@@ -1,38 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'avatarProfile.dart';
 
 class AvatarModal {
-  showImageSelection(BuildContext context) {
+  showImageSelection(BuildContext context, DocumentSnapshot userInfo) {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext context) {
           return Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              _createTile(context, 'View Profile Picture', Icons.account_circle,
-                  _action1),
-              _createTile(context, 'Select Profile Picture', Icons.camera_alt,
-                  _action2),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('View Profile Picture'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              AvatarProfile(userInfo.data['img'])));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.camera_alt),
+                title: Text('Select Profile Picture'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
             ],
           );
         });
-  }
-
-  ListTile _createTile(
-      BuildContext context, String name, IconData icon, Function action) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(name),
-      onTap: () {
-        Navigator.pop(context);
-      },
-    );
-  }
-
-  _action1() {
-    print('action 1');
-  }
-
-  _action2() {
-    print('action 2');
   }
 }
