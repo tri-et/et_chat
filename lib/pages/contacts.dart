065ac489eb85c8822e49419418b3db5adb2fe948
Unknown <rooney.et1012@gmail.com>
2019-06-28 18:04:16 +0700

    Optimize search contacts Thinh

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -13,6 +13,7 @@ class _ContactsPageState extends State<ContactsPage> {
   FirebaseUser currentUser;
   final searchController = TextEditingController();
   String txtSearchContact = "";
+  List<DocumentSnapshot> contactsData;
   @override
   void initState() {
     searchController.addListener(() {
@@ -22,84 +23,83 @@ class _ContactsPageState extends State<ContactsPage> {
     });
     super.initState();
     _getCurrentUser();
+    fetchcontact().then((onValue) {
+      setState(() {
+        contactsData = onValue;
+      });
+    });
+  }
+
+  Future<List<DocumentSnapshot>> fetchcontact() async {
+    QuerySnapshot snapshot =
+        await Firestore.instance.collection("Users").getDocuments();
+    return snapshot.documents;
   }
 
   @override
   Widget build(BuildContext context) {
     if (currentUser != null) {
       return Scaffold(
-        appBar: AppBar(
-            backgroundColor: secondary,
-            elevation: 0.0,
-            title: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-                    child: TextField(
-                      enableInteractiveSelection: false,
-                      controller: searchController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Color.fromRGBO(255, 255, 255, .45),
-                        hintText: "Type a text",
-                        contentPadding: EdgeInsets.fromLTRB(15.0, 0, 0, 0),
-                        hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                        suffixIcon: IconButton(
-                          icon: Icon(txtSearchContact == ""
-                              ? Icons.search
-                              : Icons.close),
-                          onPressed: () {
-                            setState(() {
-                              txtSearchContact = "";
-                            });
-                            searchController.clear();
-                          },
-                        ),
-                        border: new OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(24.0),
+          appBar: AppBar(
+              backgroundColor: secondary,
+              elevation: 0.0,
+              title: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+                      child: TextField(
+                        enableInteractiveSelection: false,
+                        controller: searchController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color.fromRGBO(255, 255, 255, .45),
+                          hintText: "Type a text",
+                          contentPadding: EdgeInsets.fromLTRB(15.0, 0, 0, 0),
+                          hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                          suffixIcon: IconButton(
+                            icon: Icon(txtSearchContact == ""
+                                ? Icons.search
+                                : Icons.close),
+                            onPressed: () {
+                              setState(() {
+                                txtSearchContact = "";
+                              });
+                              searchController.clear();
+                            },
                           ),
-                          borderSide: BorderSide.none,
+                          border: new OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(24.0),
+                            ),
+                            borderSide: BorderSide.none,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.person_add),
-                  iconSize: 32.0,
-                  onPressed: () {},
-                )
-              ],
-            )),
-        body: StreamBuilder(
-          stream: Firestore.instance.collection("Users").snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    if (txtSearchContact == "") {
-                      return _buildListItem(
-                          context, snapshot.data.documents[index]);
-                    } else {
-                      if (snapshot.data.documents[index].data["userName"]
-                          .contains(txtSearchContact)) {
-                        return _buildListItem(
-                            context, snapshot.data.documents[index]);
-                      } else {
-                        return Container();
-                      }
-                    }
-                  });
-            } else {
-              return CircularProgressIndicator();
-            }
-          },
-        ),
-      );
+                  IconButton(
+                    icon: Icon(Icons.person_add),
+                    iconSize: 32.0,
+                    onPressed: () {},
+                  )
+                ],
+              )),
+          body: ListView.builder(
+              itemCount: contactsData.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (txtSearchContact == "") {
+                  return _buildListItem(context, contactsData[index]);
+                } else {
+                  if (contactsData[index]
+                      .data["userName"]
+                      .contains(txtSearchContact)) {
+                    return _buildListItem(context, contactsData[index]);
+                  } else {
+                    return Container();
+                  }
+                }
+              }));
     } else {
       return CircularProgressIndicator();
     }
