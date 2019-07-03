6a20283f2ee51f4cb320f980fe127d1a07185d63
John <tripham178@gmail.com>
2019-07-03 13:39:56 +0700

    delete file

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,12 +1,10 @@
 import 'dart:collection';
 
-import 'package:et_chat/widgets/avatarModal.dart';
+import 'package:et_chat/widgets/avatarMenu.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../theme.dart';
 
 class SettingPage extends StatefulWidget {
   @override
@@ -15,7 +13,6 @@ class SettingPage extends StatefulWidget {
 
 class _SettingPageState extends State<SettingPage> {
   DocumentSnapshot _userInfo;
-  AvatarModal modal;
 
   @override
   void initState() {
@@ -34,8 +31,13 @@ class _SettingPageState extends State<SettingPage> {
             Center(
               child: GestureDetector(
                 onTap: () {
-                  modal = new AvatarModal(_userInfo);
-                  return modal.showImageSelection(context);
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AvatarMenu(
+                          userInfo: _userInfo,
+                        );
+                      });
                 },
                 child: CircleAvatar(
                   backgroundImage: NetworkImage(_userInfo.data["img"]),
@@ -43,8 +45,20 @@ class _SettingPageState extends State<SettingPage> {
                 ),
               ),
             ),
-            RaisedButton(
-              onPressed: () {
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.keyboard),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              title: Text('QR Code'),
+              onTap: () {
+                print('1235656');
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.power_settings_new),
+              title: Text('Sign Out'),
+              onTap: () {
                 Map<String, String> userInfo = new HashMap();
                 userInfo["status"] = "offline";
                 Firestore.instance
@@ -54,28 +68,6 @@ class _SettingPageState extends State<SettingPage> {
                 FirebaseAuth.instance.signOut();
                 Navigator.pushReplacementNamed(context, "/");
               },
-              padding: EdgeInsets.fromLTRB(25.0, 11.0, 25.0, 11.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Icon(
-                    Icons.power_settings_new,
-                    color: Color.fromRGBO(28, 28, 28, .46),
-                    size: 24.0,
-                  ),
-                  Text(
-                    "SIGN OUT",
-                    style: TextStyle(
-                        color: Color.fromRGBO(245, 238, 238, 1),
-                        fontSize: 17.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              color: secondary,
             ),
           ],
         ),
---
+++ lib/widgets/avatarMenu.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import 'avatarProfile.dart';
+
+class AvatarMenu extends StatelessWidget {
+  final DocumentSnapshot userInfo;
+
+  AvatarMenu({this.userInfo});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text('View Profile Picture'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AvatarProfile(userInfo.data['img'])));
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.camera_alt),
+          title: Text('Take a picture'),
+          onTap: () async {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.photo),
+          title: Text('Select a picture'),
+          onTap: () async {
+            Navigator.pop(context);
+          },
+        )
+      ],
+    );
+  }
+}
