5e15fe5defcfa1b8672a221f8748da9994a4a6dc
John <tripham178@gmail.com>
2019-07-10 10:31:22 +0700

    update code

--- lib/widgets/avatarMenu.dart
+++ lib/widgets/avatarMenu.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'avatarProfile.dart';
 
 class AvatarMenu extends StatelessWidget {
   final DocumentSnapshot userInfo;
+  final ImageSelected onImageSelected;
 
-  AvatarMenu({this.userInfo});
+  AvatarMenu({this.userInfo, this.onImageSelected});
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,18 +30,32 @@ class AvatarMenu extends StatelessWidget {
         ListTile(
           leading: Icon(Icons.camera_alt),
           title: Text('Take a picture'),
-          onTap: () {
+          onTap: () async {
             Navigator.pop(context);
+            File img = await _getImgFromCamera();
+            onImageSelected(img);
           },
         ),
         ListTile(
           leading: Icon(Icons.photo),
           title: Text('Select a picture'),
-          onTap: () {
+          onTap: () async {
             Navigator.pop(context);
+            File img = await _getImgFromgallery();
+            onImageSelected(img);
           },
         )
       ],
     );
   }
+
+  Future _getImgFromCamera() async {
+    return await ImagePicker.pickImage(source: ImageSource.camera);
+  }
+
+  Future _getImgFromgallery() async {
+    return await ImagePicker.pickImage(source: ImageSource.gallery);
+  }
 }
+
+typedef ImageSelected = void Function(File image);
